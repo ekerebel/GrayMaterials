726f3527f99b13aed6d8b001b44a56c5dc9f9262
graymaterials <erwan@kerebel.com>
2014-12-05 01:05:30 +0000

    img local for dev, dp for prod

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,12 @@
 class Listing < ActiveRecord::Base
-    has_attached_file :image, :styles => { :medium => "200x", :thumb => "80x>" }, :default_url => "NoImage.png",
-     :storage => :dropbox,
-      :dropbox_credentials => Rails.root.join("config/dropbox.yml")
-    validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+    if Rails.env.development?
+        has_attached_file :image, :styles => { :medium => "200x", :thumb => "80x>" }, :default_url => "NoImage.png"
+        validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+    else
+        has_attached_file :image, :styles => { :medium => "200x", :thumb => "80x>" }, :default_url => "NoImage.png",
+         :storage => :dropbox,
+          :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+          :path=>":style/:id_:filename"
+        validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+    end
 end
